ef0ae6f83f2e16537e3549b8cb8aaa9b912d2670
Sanidhya Singh <ssanidhya0407@gmail.com>
2025-06-09 23:59:07 +0530

    connection success

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB and start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
